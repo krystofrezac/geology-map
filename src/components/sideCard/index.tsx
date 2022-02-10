9331de98b752ebf7218b103632ffb5f02afc6d4e
Kryštof Řezáč <krystofrezac@gmail.com>
2022-02-10 15:56:33 +0100

    Remove margin so map is clickable

--- src/components/sideCard/index.tsx
+++ src/components/sideCard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const SideCardIndex: React.FC = props => (
-  <div className="p-4 m-8 w-96 bg-white shadow-2xl card">{props.children}</div>
+  <div className="p-4 m-8 ml-0 w-96 bg-white shadow-2xl card">
+    {props.children}
+  </div>
 );
 
 export const SideCardTitle: React.FC = props => (
